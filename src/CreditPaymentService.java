363269909ffa5ce43301d0491c57b4e12c76c7af
Vladislav Hudasov <vladislavhudasov@yandex.ru>
2022-09-26 18:00:17 +0300

    Full names + add 3 years

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
-public int calculate(int creditSum, int term, double percentage) {
-    double percentageMonthly = percentage / 100 / 12;
-    double percentageMonthlyTerm = Math.pow(1+percentageMonthly, term);
-    double creditPayment = creditSum * (percentageMonthly + (percentageMonthly / (percentageMonthlyTerm - 1)));
-    return (int) creditPayment;
-}
+    public int calculate(int creditSum, int term, double percentage) {
+        double percentageMonthly = percentage / 100 / 12;
+        double percentageMonthlyTerm = Math.pow(1 + percentageMonthly, term);
+        double creditPayment = creditSum * (percentageMonthly + (percentageMonthly / (percentageMonthlyTerm - 1)));
+        return (int) creditPayment;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        CreditPaymentService service = new CreditPaymentService();
-int cs = 1_000_000;
-int years = 1;
-int t = years * 12;
-double p = 9.99;
-        int cp = service.calculate(cs, t, p);
-        System.out.println("Сумма вашего кредита " + cs + "₽");
+        CreditPaymentService serviceOneYear = new CreditPaymentService();
+        int creditSum = 1_000_000;
+        int years = 1;
+        int term = years * 12;
+        double percentage = 9.99;
+        int creditPayment = serviceOneYear.calculate(creditSum, term, percentage);
+        System.out.println("Сумма вашего кредита " + creditSum + "₽");
         System.out.println("Срок кредита - " + years + " год(года)");
-        System.out.println("При процентной ставке в " + p + "%");
-        System.out.println("Ежемесячный платёж составит " + cp + "₽");
+        System.out.println("При процентной ставке в " + percentage + "%");
+        System.out.println("Ежемесячный платёж составит " + creditPayment + "₽");
+
+        CreditPaymentService serviceTwoYears = new CreditPaymentService();
+        creditSum = 1_000_000;
+        years = 2;
+        term = years * 12;
+        percentage = 9.99;
+        creditPayment = serviceTwoYears.calculate(creditSum, term, percentage);
+        System.out.println("");
+        System.out.println("Сумма вашего кредита " + creditSum + "₽");
+        System.out.println("Срок кредита - " + years + " года(лет)");
+        System.out.println("При процентной ставке в " + percentage + "%");
+        System.out.println("Ежемесячный платёж составит " + creditPayment + "₽");
+
+        CreditPaymentService serviceThreeYears = new CreditPaymentService();
+        creditSum = 1_000_000;
+        years = 3;
+        term = years * 12;
+        percentage = 9.99;
+        creditPayment = serviceThreeYears.calculate(creditSum, term, percentage);
+        System.out.println("");
+        System.out.println("Сумма вашего кредита " + creditSum + "₽");
+        System.out.println("Срок кредита - " + years + " года(лет)");
+        System.out.println("При процентной ставке в " + percentage + "%");
+        System.out.println("Ежемесячный платёж составит " + creditPayment + "₽");
     }
 }
